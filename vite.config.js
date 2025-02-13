600704c71f8b031b584754d0d53d6efc276d18f3
Harshitasingh12896 <shikhasingh70869@gmail.com>
2025-02-13 12:42:19 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: "/Home-estate/", // Replace with your repo name
   plugins: [react()],
 });
 
