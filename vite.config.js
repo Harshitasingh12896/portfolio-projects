6807e5da5fb092901a9a97e6c916a6a27d1ab6a8
Harshitasingh12896 <shikhasingh70869@gmail.com>
2025-02-13 12:51:17 +0530

    Configured for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/Home-estate/", // Replace with your repo name
+  base: "/Home-Estate/", // Replace with your repo name
   plugins: [react()],
 });
 
